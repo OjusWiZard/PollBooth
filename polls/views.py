3c216c9dcb855cdf9a64b280867b0cc923c26fe1
OjusWiZard <ojuswimail@gmail.com>
2020-06-29 21:17:53 +0530

    added seperate views for seperate features

--- polls/views.py
+++ polls/views.py
@@ -4,3 +4,15 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello World, views.py")
+
+
+def vote(request, question_id):
+    return HttpResponse(f'You are voting on question "{question_id}"')
+
+
+def detail(request, question_id):
+    return HttpResponse(f'You are looking at "{question_id}"')
+
+
+def result(request, question_id):
+    return HttpResponse(f'You are looking at result of "{question_id}"')
