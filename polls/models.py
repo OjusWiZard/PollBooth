4e423ce0a403b8a60671c8b38949ca6db306b47f
OjusWiZard <ojuswimail@gmail.com>
2020-06-30 00:38:57 +0530

    added result.html template

--- polls/models.py
+++ polls/models.py
@@ -5,13 +5,13 @@ class Question(models.Model):
     question_text = models.CharField(max_length=128)
     pub_date = models.DateTimeField('date published')
 
-    # def __str__(self):
-    #     return self.question_text
+    def __str__(self):
+        return self.question_text
 
 
 class Choice(models.Model):
     choice_text = models.CharField(max_length=32)
-    vote = models.IntegerField(default=0)
+    votes = models.IntegerField(default=0)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
 
     def __str__(self):
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
-from django.http import HttpResponse
-from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 
 def index(request):
@@ -11,7 +11,15 @@ def index(request):
 
 
 def vote(request, question_id):
-    return HttpResponse(f'You are voting on question "{question_id}"')
+    question = get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError,Choice.DoesNotExist):
+        render(request, 'polls/detail.html', {'question':question, 'error_message':"You didn't select any choice"})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results',args=(question_id,)))
 
 
 def detail(request, question_id):
@@ -20,4 +28,4 @@ def detail(request, question_id):
 
 
 def result(request, question_id):
-    return HttpResponse(f'You are looking at result of "{question_id}"')
+    return render(request, 'polls/result.html', {'question':question})
