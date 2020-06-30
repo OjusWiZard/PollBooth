8c41d7b69a0139c440be5f19351175fc92e4d503
OjusWiZard <ojuswimail@gmail.com>
2020-06-30 16:54:48 +0530

    customized the Administration

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,20 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+admin.AdminSite.site_header = 'PollBooth Administration'
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 0
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields':['question_text']}),
+        ('Date Information', {'fields':['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
