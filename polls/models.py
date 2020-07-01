b1e19ee76d096e719a20202db70eb550c75f36ff
OjusWiZard <ojuswimail@gmail.com>
2020-07-01 15:18:13 +0530

    Added user model

--- polls/models.py
+++ polls/models.py
@@ -16,3 +16,10 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+class User(models.Model):
+    username = models.CharField(max_length=32)
+    password = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.username
